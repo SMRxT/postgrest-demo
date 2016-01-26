4a8a5b1c53f5bec3cde7149792a0ad5c58610b8d
AndrewRademacher <andrew.rademacher@smrxt.com>
2016-01-25 18:39:21 -0600

    Added testing for failed private logs table.

--- db/verify/postgrest/tables/private-logs.sql
+++ db/verify/postgrest/tables/private-logs.sql
@@ -2,6 +2,16 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+SET search_path TO postgrest, public;
+
+DO $$
+DECLARE
+   rstr text;
+BEGIN
+   rstr := register_account('andrew.rademacher@smrxt.com', 'Andrew Rademacher', '12345');
+   EXECUTE format('SET ROLE %s;', rstr);
+
+   INSERT INTO private_log(body) VALUES ('This is my test log entry.');
+END$$;
 
 ROLLBACK;
