fa839b3e3ae14dddbf41d85edd2f25d88edb1791
AndrewRademacher <andrew.rademacher@smrxt.com>
2016-01-25 18:34:13 -0600

    Verified get_current_account_id() function.

--- db/verify/postgrest/functions/get-current-account-id.sql
+++ db/verify/postgrest/functions/get-current-account-id.sql
@@ -2,6 +2,23 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+SET search_path TO postgrest, public;
+
+DO $$
+DECLARE
+   rstr text;
+   tmpaid uuid;
+BEGIN
+   rstr := register_account('andrew.rademacher@smrxt.com', 'Andrew Rademacher', '12345');
+
+   EXECUTE format('SET ROLE %s;', rstr);
+   
+   SELECT account_id INTO tmpaid
+   FROM account;
+
+   IF NOT (tmpaid = get_current_account_id()) THEN 
+      RAISE EXCEPTION 'Account id from function and table are not equal.';
+   END IF;
+END$$;
 
 ROLLBACK;
