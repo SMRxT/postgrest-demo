ee77196374d0e8bbb709a6ef19b9c84dd59b7d5a
AndrewRademacher <andrew.rademacher@smrxt.com>
2016-01-26 18:50:35 -0600

    Fixed HTTP test wrapper.

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,4 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Lens
+import           Data.Maybe
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.URL
+import           System.IO
+import           System.IO.Unsafe
+import           System.Process
+import           Test.Tasty
+import           Test.Tasty.HUnit               as HUnit
+import           Test.Tasty.QuickCheck          as QC
+
+data TestEnv
+   = TestEnv !Connection !(Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
+
 main :: IO ()
-main = putStrLn "hello world"
+main = defaultMain
+   $ withResource initEnv freeEnv tests
+
+initEnv :: IO TestEnv
+initEnv = do
+   pgConn <- connect $ fromJust
+      $ parseDatabaseUrl "postgres://localhost:5432/postgrest_demo"
+
+   depProc@(_, _, _, depPH) <- createProcess
+      (shell "cd ../db && sqitch deploy")
+   depECode <- waitForProcess depPH
+   print depECode
+
+   verProc@(_, _, _, verPH) <- createProcess
+      (shell "cd ../db && sqitch verify")
+   verECode <- waitForProcess verPH
+   print verECode
+
+   wsProc@(_, _, _, wsPH) <- createProcess $
+      (shell "postgrest postgres://andrew:@localhost:5432/postgrest_demo -a postgrest_anonymous -s postgrest")
+         { std_in  = UseHandle stdin
+         , std_out = UseHandle stdout
+         , std_err = UseHandle stderr
+         }
+
+   return $ TestEnv pgConn wsProc
+
+freeEnv :: TestEnv -> IO ()
+freeEnv (TestEnv pgConn (_, _, _, wsPH)) = do
+   terminateProcess wsPH
+
+   revProc@(_, _, _, revPH) <- createProcess
+      (shell "cd ../db && sqitch revert -y")
+   revECode <- waitForProcess revPH
+   print revECode
+
+--
+
+tests :: IO TestEnv -> TestTree
+tests getEnv = testGroup "HTTP Tests"
+   [
+   ]
