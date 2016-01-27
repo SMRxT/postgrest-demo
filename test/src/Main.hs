ee4bb14f155a9a4fd3f1386418d5404abdb102bf
AndrewRademacher <andrew.rademacher@smrxt.com>
2016-01-27 00:21:19 -0600

    Tested user register route.

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,13 +1,29 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable     #-}
+{-# LANGUAGE DeriveGeneric          #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE TemplateHaskell        #-}
 
 module Main where
 
 import           Control.Concurrent
 import           Control.Concurrent.Async
 import           Control.Lens
+import           Data.Aeson
+import           Data.Aeson.Casing
+import           Data.Aeson.Lens
 import           Data.Maybe
+import           Data.Set                       (Set)
+import qualified Data.Set                       as Set
+import           Data.Text                      (Text)
+import           Data.Typeable
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.URL
+import           GHC.Generics
+import           Network.Wreq
+import           Network.Wreq.Types
 import           System.IO
 import           System.IO.Unsafe
 import           System.Process
@@ -15,8 +31,49 @@ import           Test.Tasty
 import           Test.Tasty.HUnit               as HUnit
 import           Test.Tasty.QuickCheck          as QC
 
+type Account = Text
+
 data TestEnv
-   = TestEnv !Connection !(Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
+   = TestEnv
+      { envPostgresConnection :: !Connection
+      , envWebserverHandles   :: !(Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
+      , envAccountSet         :: MVar (Set Account)
+      }
+
+-- BEGIN: Messages
+
+data RegisterPostReq
+   = RegisterPostReq
+      { regpostreqEmail    :: Text
+      , regpostreqName     :: Text
+      , regpostreqPassword :: Text
+      }
+   deriving (Show, Typeable, Generic)
+
+instance ToJSON RegisterPostReq where
+   toJSON = genericToJSON $ aesonPrefix snakeCase
+instance FromJSON RegisterPostReq where
+   parseJSON = genericParseJSON $ aesonPrefix snakeCase
+instance Postable RegisterPostReq where
+   postPayload = postPayload . toJSON
+
+data RegisterPostRes
+   = RegisterPostRes
+      { regpostresRegisterAccount :: Text
+      }
+   deriving (Show, Typeable, Generic)
+
+instance ToJSON RegisterPostRes where
+   toJSON = genericToJSON $ aesonPrefix snakeCase
+instance FromJSON RegisterPostRes where
+   parseJSON = genericParseJSON $ aesonPrefix snakeCase
+
+$(makeLensesWith abbreviatedFields ''RegisterPostReq)
+$(makeLensesWith abbreviatedFields ''RegisterPostRes)
+
+-- END: Messages
+
+$(makeLensesWith abbreviatedFields ''TestEnv)
 
 main :: IO ()
 main = defaultMain
@@ -44,10 +101,13 @@ initEnv = do
          , std_err = UseHandle stderr
          }
 
-   return $ TestEnv pgConn wsProc
+   threadDelay 500000
+
+   as <- newMVar Set.empty
+   return $ TestEnv pgConn wsProc as
 
 freeEnv :: TestEnv -> IO ()
-freeEnv (TestEnv pgConn (_, _, _, wsPH)) = do
+freeEnv (TestEnv pgConn (_, _, _, wsPH) _) = do
    terminateProcess wsPH
 
    revProc@(_, _, _, revPH) <- createProcess
@@ -59,5 +119,14 @@ freeEnv (TestEnv pgConn (_, _, _, wsPH)) = do
 
 tests :: IO TestEnv -> TestTree
 tests getEnv = testGroup "HTTP Tests"
-   [
+   [ testCase "Register User" $ caseRegisterUser getEnv
    ]
+
+caseRegisterUser :: IO TestEnv -> IO ()
+caseRegisterUser getEnv = do
+   env <- getEnv
+   res <- post "http://localhost:3000/rpc/register_account"
+      (RegisterPostReq "andrew.rademacher@smrxt.com" "Andrew Rademacher" "12345")
+   case res ^? responseBody . nth 0 . _JSON . (registerAccount :: Lens' RegisterPostRes Text) of
+      Nothing -> assertFailure "Response did not contain account name."
+      Just  a -> modifyMVar_ (env ^. accountSet) (return . Set.insert a)
